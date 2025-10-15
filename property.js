c853657816022d18b83df2bcf024dfca9cd5146f
plabondas001 <plabondas001@gmail.com>
2025-10-15 22:32:32 +0600

    property

---
+++ property.js
@@ -0,0 +1,12 @@
+const person ={
+    name: 'Plabon',
+    address: 'Dhaka',
+    age:20,
+    profession:'Developer',
+    selary: 100000,
+    married: false
+}
+// const income = person.selary;
+// console.log(person['age']);
+const dam = person['selary'];
+console.log(dam);
