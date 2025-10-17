6e73f922998a3414612ece672a7f21fc1d4e5141
plabondas001 <plabondas001@gmail.com>
2025-10-17 12:04:03 +0600

    values

---
+++ keys.js
@@ -0,0 +1,12 @@
+const computer = {
+    monitor: 'hp',
+    ram: '16 GB',
+    sdd: '512 GB',
+    processor: 'intel',
+    price: 60000,
+    isGood: true
+}
+const keys = Object.keys(computer);
+console.log(keys);
+
+
---
+++ property_values.js
@@ -0,0 +1,10 @@
+const person ={
+    name: 'Plabon',
+    address: 'Dhaka',
+    age:20,
+    profession:'Developer',
+    selary: 100000,
+    married: false
+}
+const values = Object.values(person);
+console.log(values);
